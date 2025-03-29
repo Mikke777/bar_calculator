a6c2095a4c2f91709530c5c352a2e0e358d48ad4
michele di stadio <distadiomichele93@gmail.com>
2025-03-29 16:37:14 +0100

    Builded the first component, the sidebar with the products, fix on the seed file to ensure the correct format for the price

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -30,7 +30,7 @@ module Backend
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:3000"
+        origins "http://localhost:5173"
         resource "*",
                  headers: :any,
                  methods: %i[get post put patch delete options head]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,17 @@
-// filepath: /frontend/src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
-import CartPage from "./pages/CartPage";
+import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <div style={{ display: "flex" }}>
-        {/* Sidebar */}
+      <div  id="root">
         <Sidebar />
-
-        {/* Main Content */}
-        <div style={{ flex: 1, padding: "20px" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Routes>
+        <div className="main-content">
+          <Home />
         </div>
       </div>
-    </Router>
-  );
+      );
 };
 
 export default App;
